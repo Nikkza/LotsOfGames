89f809d4292c763fbe8bf423b75fcc3f15c2d659
Nicklas Isaksson <isaksson.nicklas@gmail.com>
2020-09-14 11:22:36 +0200

    chnages in main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Games games = new Games();
-        GameModel gameModel = new GameModel();
-        var array = games.CreateArray("10");
-        if (array.size() > 0)
-        {
-            var resultsFromGame = games.TotalAmountOfPoints(array, gameModel);
-            System.out.println(resultsFromGame.WriteOutGameResults(resultsFromGame));
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter how many games you want: ");
+            String gameValue = scanner.next();
+            Games games = new Games();
+            GameModel gameModel = new GameModel();
+            List<String> array = games.CreateArray(gameValue);
+            if (array.size() > 0) {
+                GameModel resultsFromGame = games.TotalAmountOfPoints(array, gameModel);
+                System.out.println(resultsFromGame.WriteOutGameResults(resultsFromGame));
+            } else {
+                System.out.println("Nothing was entered. Please try again");
+            }
         }
     }
 }
